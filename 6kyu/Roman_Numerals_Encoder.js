6548072610131e817b0bd0328ed42fb2d0d2e1d1
Ryan A, Jones <ryanajonesdev@gmail.com>
2021-07-14 14:44:33 -0700

    Added algorithms - 'Roman_Numerals_Encoder'

---
+++ 6kyu/Roman_Numerals_Encoder.js
@@ -0,0 +1,25 @@
+const romanNum = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+const romanNumerals = (num) => {
+  let result = '';
+  for (const key of Object.keys(romanNum)) {
+    result += key.repeat(Math.floor(num / romanNum[key]));
+    num %= romanNum[key];
+  }
+  return result;
+};
+romanNumerals(1990);
